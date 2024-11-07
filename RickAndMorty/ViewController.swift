f6c1f11cb879459d2ae0bd8e3719bc20f2795e21
Ivan Rybkin <i.rybkin@icloud.com>
2024-11-07 14:11:23 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Ivan Rybkin on 07.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
