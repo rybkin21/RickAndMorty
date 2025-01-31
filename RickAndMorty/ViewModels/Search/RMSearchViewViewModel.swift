8fcea619e46a73b8fefb93f16cc8fc2d89966713
Ivan Rybkin <i.rybkin@icloud.com>
2025-01-31 21:06:34 +0300

    Hook up Character & Episode searching

--- RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -18,6 +18,8 @@ final class RMSearchViewViewModel {
 
     private var noResultsHandler: (() -> Void)?
 
+    private var searchResultModel: Codable?
+    
     // MARK: - Init
 
     init(config: RMSearchViewController.Config) {
@@ -107,6 +109,7 @@ final class RMSearchViewViewModel {
         }
 
         if let results = resultsVM {
+            self.searchResultModel = model
             self.searchResultHandler?(results)
         }
         else {
@@ -133,5 +136,12 @@ final class RMSearchViewViewModel {
         _ block: @escaping ((RMSearchInputViewViewModel.DynamicOption, String)) -> Void
     ) {
         self.optionMapUpdateBlock = block
+    }
+
+    public func locationSearchResult(at index: Int) -> RMLocation? {
+        guard let searchModel = searchResultModel  as? RMGetAllLocationsResponse else {
+            return nil
+        }
+        return searchModel.results[index]
     }
 }
--- RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/Views/Search/RMSearchView.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 protocol RMSearchViewDelegate: AnyObject {
-    func rmSearchView(_ searchView: RMSearchView,
-                      didSelectOption option: RMSearchInputViewViewModel.DynamicOption)
+    func rmSearchView(_ searchView: RMSearchView, didSelectOption option: RMSearchInputViewViewModel.DynamicOption)
+
+    func rmSearchView(_ searchView: RMSearchView, didSelectLocation location: RMLocation)
 }
 
 final class RMSearchView: UIView {
@@ -43,6 +44,8 @@ final class RMSearchView: UIView {
         searchInputView.delegate = self
 
         setUpHandlers(viewModel: viewModel)
+
+        resultsView.delegate = self
     }
     
     required init?(coder: NSCoder) {
@@ -130,5 +133,14 @@ extension RMSearchView: RMSearchInputViewDelegate {
 
     func rmSearchInputViewDidTapSearchKeyboardButton(_ inputView: RMSearchInputView) {
         viewModel.executeSearch()
+    }
+}
+
+extension RMSearchView: RMSearchResultsViewDelegate {
+    func rmSearchResultsView(_ resultsView: RMSearchResultsView, didTapLocationAt index: Int) {
+        guard let locationModel = viewModel.locationSearchResult(at: index) else {
+            return
+        }
+        delegate?.rmSearchView(self, didSelectLocation: locationModel)
     }
 }
